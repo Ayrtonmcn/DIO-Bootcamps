e3446e217b147e943ce6554b76b5daf911405b41
Ayrtonmcn <ayrtonmcn@gmail.com>
2021-08-16 18:24:23 -0300

    Resolucao dos Desafios Aritmeticos em Java

---
+++ src/DesafioAritmeticoEmJava/Desafio02AbaixoDaDiagonalSecundaria.java
@@ -0,0 +1,37 @@
+package DesafioAritmeticoEmJava;
+
+/*
+Leia um caractere maiúsculo, que indica uma operação que deve ser realizada e uma matriz M[12][12].
+Em seguida, calcule e mostre a soma ou a média considerando somente aqueles elementos que estão abaixo da
+diagonal Secundária da matriz, conforme ilustrado abaixo (área verde).
+ */
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Desafio02AbaixoDaDiagonalSecundaria {
+
+    public static void main(String[] args) throws IOException {
+        Scanner leitor = new Scanner(System.in);
+
+        double soma = 0;
+        char O = leitor.next().toUpperCase().charAt(0);
+        double[][] M = new double[12][12];
+
+        for (int i = 0; i < M.length; i++) {
+            for (int j = 0; j < M.length; j++) {
+                M[i][j] = leitor.nextDouble();
+            }
+        }
+
+        for (int i = M.length - 1; i > 0; --i) {
+            for (int j = M.length - 1; j >= M.length - i; --j) {
+                soma += M[i][j];
+            }
+        }
+
+        if (O == 'M') soma /= ((M.length * M.length) - 12) / 2;
+
+        System.out.println(String.format("%.1f", soma));
+    }
+}
